6016faf4bab35c33be2592bcc88ae1c77b076c74
Fel <felgonzalez951@gmail.com>
2024-07-26 14:18:34 -0600

    updated for tailwind compatability, might be scrapped

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/main.jsx",
+    "./src/App.jsx",
+    "./src/pages/AboutMe.jsx",
+    "./src/pages/ContactMe.jsx",
+    "./src/pages/Portfolio.jsx",
+    "./src/pages/Resume.jsx",
+    "./src/pages/Resume.jsx",
+    "./src/components/Nav.jsx",
+    "./src/components/UI/Navbar.jsx",
+    "./src/components/UI/ListItem.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
