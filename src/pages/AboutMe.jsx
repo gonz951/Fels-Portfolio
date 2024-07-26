78fb1e91045dfa19566b9336e4ae6803b64c373a
Fel <felgonzalez951@gmail.com>
2024-07-26 17:09:28 -0600

    started adding about me summary + picture

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,11 +1,15 @@
 import '../components/styles/pages.css';
+import urchin from '../components/assets/urchin.png';
 
 export default function AboutPage() {
     return (
         <div className='card'>
+            <div>
+            <img src={urchin} alt='me' className='urchin'></img>
+            </div>
          <h2>About Me</h2>
          <p>
-            Add stuff later. I just need text here, right now. 
+            Paula 'Fel' Gonzalez is a student fresh out of the EdX Web Development Bootcamp. With no future educational endeavours in mind, there is nothing holding her back from working immediately. With the bootcamp's help, Fel is adept at working in a group/team setting, even with those she has not worked with before. If there's an overall goal in mind, 
          </p>
         </div>
     );
