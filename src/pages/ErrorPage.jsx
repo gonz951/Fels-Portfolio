fe96d373e621019d611eb54fad2d9399019cb88c
Fel <felgonzalez951@gmail.com>
2024-07-26 17:56:17 -0600

    added styling to Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,15 @@
 import { useRouteError } from 'react-router-dom';
+import '../components/styles/pages.css'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div id="error-page" className='card'>
       <h2>Oops!</h2>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      <p className='darker'>Sorry, an unexpected error has occurred.</p>
+      <p className='darker'>
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
