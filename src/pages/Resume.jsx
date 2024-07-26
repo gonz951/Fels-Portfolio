ab3cfd8849f03d53bea0a72e7c374d2ee04429e8
Fel <felgonzalez951@gmail.com>
2024-07-26 17:09:07 -0600

    added download link and proficiencies content

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,8 +5,28 @@ export default function ResumePage() {
         <div className='card'>
          <h2>Resume</h2>
          <p>
-            Add stuff later. I just need text here, right now. 
-         </p>
+            A resume is available to be <a href='https://drive.google.com/uc?export=download&id=15-nzN9JM-fUKOfS3lDthf8wrgKheAYm1'>downloaded</a> if needed. However, a brief list of skills might give you a good idea of what's in there.
+        </p>
+        <p>
+            <h3>Front-End Skills:</h3>
+            <ul className='skills'>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>Mobile Responsive design</li>
+                <li>Javascript</li>
+                <li>React</li>
+            </ul>
+            <h3>Back-End Skills:</h3>
+            <ul className='skills'>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL/Sequelize</li>
+                <li>MongoDB/Mongoose</li>
+                <li>GraphQL</li>
+            </ul>
+        </p>
         </div>
     );
 }
