9b6ca02058ba2ff16e5715554228b7e59d796dda
Fel <felgonzalez951@gmail.com>
2024-09-05 15:12:26 -0600

    notes for final project to be added in

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,8 @@
 import '../components/styles/pages.css';
 import StartLine from '../components/assets/Start-Line.png'
 import PennyPacker from '../components/assets/PennyPacker.png'
+// Make rig builder pro image
+import RigBuilderPro from '../ADD IMAGE !!'
 
 export default function PortfolioPage() {
     return (
@@ -15,6 +17,7 @@ export default function PortfolioPage() {
                 {/* Add repo links later */}
                 <a href='https://pennypacker.onrender.com/' target='_blank'><img src={PennyPacker} className='web-img' alt='PennyPacker website image'/></a>
                 {/* NEED TO ADD MORE PROJECTS */}
+                <a href='https://rig-builderpro-a6ui.onrender.com/' target='_blank'><img src={RigBuilderPro} className='web-img' alt='Rig-Builder Pro website image'/></a>
             </p>
          </div>
         </div>
