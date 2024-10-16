c9655137d43bc5f04eae72c4e381a4ea149367d4
Fel <felgonzalez951@gmail.com>
2024-10-16 14:27:48 -0600

    updated 'about me' page description

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
             </div>
          <h2>About Me</h2>
          <p className='darker'>
-            Paula 'Fel' Gonzalez is a student fresh out of the EdX Web Development Bootcamp. With no future educational endeavours in mind, there is nothing holding her back from working immediately. With the bootcamp's help, Fel is adept at working in a group/team setting, even with those she has not worked with before. If there's an overall goal in mind, she'll work to achieve it no matter the team. Even outside the bootcamp, Fel has worked in an environment that required team building and proper communication in a working environment.
+            Paula 'Fel' Gonzalez is a student fresh out of the EdX Web Development Bootcamp. With her brand new skills and willingness to learn, there is nothing holding her back from working immediately. With the bootcamp's help, Fel is adept at working in a group/team setting, even with those she has not worked with before. If there is an overall goal in mind, she will work to achieve it no matter the team. Even outside the bootcamp, Fel has worked in an environment that required team building and proper communication in a working environment.
          </p>
          <p className='darker'>
              That being said, even with her short education, she's more than willing to learn more and expand her skills to fit any new positions she may want, or her employer wants her, to fill. 
